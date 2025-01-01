a4d6f045e5c1911485f9f5ed5f48c9aa88a6e639
liukefu2050 <liukefu2050@sina.com>
2025-01-01 14:37:12 +0800

    init

---
+++ demo1/test.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <memory>
+
+
+using namespace std;
+
+class Test :public enable_shared_from_this<Test>
+{
+public:
+	Test() {}
+	~Test() {}
+	shared_ptr<Test> getPtr()
+	{
+		return shared_from_this();
+	}
+
+	void set(int id, string name)
+	{
+		_id = id;
+		_name = name;
+	}
+
+	int getId()
+	{
+		return _id;
+	}
+
+	string getName()
+	{
+		return _name;
+	}
+private:
+	string _name;
+	int _id;
+};
+
+
+int main()
+{
+	shared_ptr<Test>t1 = make_shared<Test>();
+	t1->set(1, "test");
+	cout << "id:" << t1->getId() << endl;
+	cout << "name:" << t1->getName() << endl;
+	return 0;
+}
